aada69df641bfde80bf3b2b4210217247e97c0c7
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-12-03 22:17:53 -0800

    Initial Commit.

--- main.py
+++ main.py
@@ -8,21 +8,31 @@ app = Flask(__name__)
 
 
 def get_fact():
-
     response = requests.get("http://unkno.com")
-
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
+    return " ".join(facts[0].getText().split())
 
-    return facts[0].getText()
+
+def get_pig_latinized_url(fact):
+    url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    data = {'input_text': fact}
+    r = requests.post(url, data=data, allow_redirects=False)
+    return r.headers['Location']
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    url = get_pig_latinized_url(fact)
+    body = """
+        <html>Click Me...</html>
+        <br>
+        <a href="{0}">{0}</a><br>
+    """.format(url)
+    return Response(response=body, mimetype='text/html')
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
